4099f15fd8b796ab1aa2ca2c163a3c053a24a3dd
John Nunley <dev@notgull.net>
2023-12-29 12:58:41 -0800

    CI fixups
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,39 @@
 // MIT/Apache2 License
 
 //! Integrations between [`smol`] and [`axum`].
-//! 
+//!
 //! By default, [`axum`] only supports the [`tokio`] runtime. This crate adds a `serve`
 //! function that can be used with [`smol`]'s networking types.
-//! 
+//!
 //! ## Examples
-//! 
+//!
 //! ```no_run
 //! use async_io::Async;
 //! use axum::{response::Html, routing::get, Router};
 //! use macro_rules_attribute::apply;
-//! 
+//!
 //! use std::io;
 //! use std::net::TcpListener;
 //! use std::sync::Arc;
-//! 
+//!
 //! #[apply(smol_macros::main!)]
 //! async fn main(ex: &Arc<smol_macros::Executor<'_>>) -> io::Result<()> {
 //!     // Build our application with a route.
 //!     let app = Router::new().route("/", get(handler));
-//! 
+//!
 //!     // Create a `smol`-based TCP listener.
 //!     let listener = Async::<TcpListener>::bind(([127, 0, 0, 1], 3000)).unwrap();
 //!     println!("listening on {}", listener.get_ref().local_addr().unwrap());
-//! 
+//!
 //!     // Run it using `smol_axum`
 //!     smol_axum::serve(ex.clone(), listener, app).await
 //! }
-//! 
+//!
 //! async fn handler() -> Html<&'static str> {
 //!     Html("<h1>Hello, World!</h1>")
 //! }
 //! ```
-//! 
+//!
 //! [`axum`]: https://crates.io/crates/axum
 //! [`smol`]: https://crates.io/crates/smol
 //! [`tokio`]: https://crates.io/crates/tokio
